d86008c5c82faf1f8990e3e640881066bf57a086
tanishq <tanishq.shukla.cd.che21@iitbhu.ac.in>
2024-01-06 13:02:41 +0530

    userHandler.ts userService.ts and response.ts created with multiple handlers, services and responseformats

--- user-service/app/handlers/userHandler.ts
+++ user-service/app/handlers/userHandler.ts
@@ -1,15 +1,28 @@
-import { APIGatewayProxyCallbackV2 } from "aws-lambda";
+import { APIGatewayProxyCallbackV2, APIGatewayProxyEventV2 } from "aws-lambda";
+import { UserService } from "./../service/userService";
 
-export const Signup = async(event : APIGatewayProxyCallbackV2) => {
-    console.log(event);
-    return {
-        statusCode : 200,
-        headers : {
-            "Access-Control-Allow-Origin" : "*",
-        },
-        body : JSON.stringify({
-            message : "Response from Signup",
-            data : {}
-        }),
-    };
+const service = new UserService();
+
+export const Signup = async(event : APIGatewayProxyEventV2) => {
+    return service.CreateUser(event);
+};
+
+export const Login = async(event : APIGatewayProxyEventV2) => {
+    return service.UserLogin(event);
+};
+
+export const Verify = async(event : APIGatewayProxyEventV2) => {
+    return service.VerifyUser(event);
+};
+
+export const Profile = async(event : APIGatewayProxyEventV2) => {
+    return service.CreateUser(event);
+};
+
+export const Cart = async(event : APIGatewayProxyEventV2) => {
+    return service.CreateUser(event);
+};
+
+export const Payment = async(event : APIGatewayProxyEventV2) => {
+    return service.CreateUser(event);
 };
--- user-service/app/service/userService.ts
+++ user-service/app/service/userService.ts
@@ -1,5 +1,21 @@
-export class UserRepository{
-    constructor(){
+import { SuccessResponse } from "app/utility/response"
+import { APIGatewayProxyEventV2 } from "aws-lambda"
+
+export class UserService{
+    constructor(){}
+
+    async CreateUser(event : APIGatewayProxyEventV2) {
+        return SuccessResponse({message : "response from CreateUser"});
+        
+    }
+
+    async UserLogin(event : APIGatewayProxyEventV2) {
+        return SuccessResponse({message : "response from UserLogin"});
+        
+    }
+
+    async VerifyUser(event : APIGatewayProxyEventV2) {
+        return SuccessResponse({message : "response from VerifyUser"});
         
     }
 }
